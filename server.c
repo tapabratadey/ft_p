c262eeb310f1a0019a6e25b5e1c40f651736ba73
tapabratadey <tapabratadey02@gmail.com>
2018-05-14 17:47:50 -0700

    check

--- server.c
+++ server.c
@@ -6,13 +6,37 @@ int error(char *str)
     exit(0);
 }
 
+// void    get_ls(t_server *server)
+// {
+
+// }
+
+// void    if_ls(t_server *server, char *buff)
+// {
+//     if (ft_strcmp(buff, "ls") == 0)
+//         get_ls(server);
+// }
+
+void    if_quit(char *buff)
+{
+    if (ft_strcmp(buff, "quit") == 0)
+        error("Disconnected.\n");
+}
+
 void    get_from_client(t_server *server)
 {
     char buff[1024];
-    while ((server->ret_recv = recv(server->server_accept, buff, 1023, 0)) > 0)
+    //server receives the msg
+    while (1)
     {
+        server->ret_recv = recv(server->server_accept, buff, 1023, 0);
         buff[server->ret_recv] = '\0';
-        
+        // if_ls(server, buff);
+        if_quit(buff);
+        printf("Clinet: %s\n", buff);
+        send (server->server_accept, buff, ft_strlen(buff), 0);
+        ft_bzero(buff, sizeof(buff));
+
     }
 }
 
@@ -20,14 +44,17 @@ void    server_loop(t_server *server)
 {
     struct sockaddr_in new_addr;
     socklen_t addr_size;
+    pid_t childpid;
     while (1)
     {
         listen(server->server_socket, 10);
         if ((server->server_accept = accept(server->server_socket, (struct sockaddr *)&new_addr, &addr_size)) < 0)
             error("Couldn't accept connection.\n");
         printf("Connection accepted.\n");//from %s:%d\n", inet_ntoa(new_addr.sin_addr), ntoa(new_addr.sin_port));
-        if (fork() == 0)
-            get_from_client(server);
+        ft_putstr("hello");
+        if ((childpid = fork()) == 0)
+            close(server->server_socket);
+        get_from_client(server);
     }
 }
 
@@ -45,7 +72,7 @@ void create_client_server(t_server *server)
         error("Couldn't bind to port.\n");
     printf("Connected to port.\n");
     server_loop(server);
-}
+}               
 
 int main(int argc, char **argv)
 {
